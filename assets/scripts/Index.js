5b083a12cb0df250bde150436c1609bea40317ba
vanderpatrick <patrick123321123@icloud.com>
2022-12-17 17:30:04 +0000

    Add form logic to populate the cards fields

--- assets/scripts/Index.js
+++ assets/scripts/Index.js
@@ -1,32 +1,23 @@
-const setData = () => {
-  let setTitle = document.getElementById("title-content").value;
-  let setContent = document.getElementById("msg-content").value;
-  let setSigned = document.getElementById("signed-content").value;
-  let title = document.getElementById("title");
-  let content = document.getElementById("content");
-  let signed = document.getElementById("signed");
-  if (setTitle.length && setContent.length && setSigned.length >= 1) {
-    content.innerHTML = setContent;
-    signed.append(` ${setSigned}`);
-    title.innerHTML = setTitle;
-  } else {
-    console.log("remove");
-  }
-};
-const background = () => {
-  let image = document.querySelector(".test");
-  image.style.backgroundImage =
-    "url('https://picsum.photos/200/300')";
-};
+// Loose vars
+const cardTitle = document.getElementById("card-title");
+const cardContent = document.getElementById("card-content");
+const cardSender = document.getElementById("card-sender");
+const bgSelect = document.getElementById("bgSelect");
+const bg = document.getElementById('testing')
+// Function to change bg images
+function changeImage(element) {
+  bg.src = "assets/images/" + element.value + ".jpg"
+}
+const createCard = () => {
+  let setTitle = document.getElementById("title").value;
+  let setContent = document.getElementById("content").value;
+  let setSender = document.getElementById("destiny").value;
 
-const resetData = () => {
-  let title = document.getElementById("title");
-  let content = document.getElementById("content");
-  let signed = document.getElementById("signed");
-  let image = document.querySelector(".test");
-  image.style.backgroundImage =
-    "url('')";
-  title.textContent = "title";
-  content.textContent = "content";
-  signed.textContent = "signed";
+  if (setTitle.length && setSender.length && setContent.length >= 1) {
+    cardTitle.innerHTML = setTitle;
+    cardContent.innerHTML = setContent;
+    cardSender.innerHTML = setSender;
+  } else {
+    console.log("missig fields");
+  }
 };
